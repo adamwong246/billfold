1f1e2305c21fdbbad6ed02ac87c4ef4b3df53ce5
Adam Wong <adamwong246@gmail.com>
2014-07-22 14:16:09 -0700

    working prototype

--- leaderboard.js
+++ leaderboard.js
@@ -1,44 +1,97 @@
-// Set up a collection to contain player information. On the server,
-// it is backed by a MongoDB collection named "players".
-
 Roommates = new Meteor.Collection("roommates");
-Bills = new Meteor.Collection("bills"); // These are divided equally amongst all roommates
+Bills     = new Meteor.Collection("bills");
+Payments  = new Meteor.Collection('payment');
+
+Handlebars.registerHelper("prettifyDate", function(date) {
+  return date.toISOString().substring(0, 10);
+});
 
 if (Meteor.isClient) {
-  Template.grid.roommates = function () {
-    return Roommates.find({});
+
+  Template.grid.roommates = function () {return Roommates.find({});};
+  Template.grid.bills     = function () {return Bills.find({});};
+  Template.grid.payments  = function () {return Payments.find({});};
+
+  Template.grid.owed = function (options) {
+    var roommate = Roommates.findOne(options.hash.roommate_id);
+    var bill = Bills.findOne(options.hash.bill_id);
+
+    if (bill && roommate ) {
+      if ((roommate.arrival_date <= bill.departure_date) && (roommate.departure_date >= bill.arrival_date)) {
+
+        // the difference in days of the bill's timespan
+        var timeDiff = Math.abs(bill.departure_date.getTime() - bill.arrival_date.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        
+        // range to calculate over
+        var lesser  = new Date(Math.max  (roommate.arrival_date, bill.arrival_date));
+        var greater = new Date(Math.min  (roommate.departure_date, bill.departure_date));
+
+        var accumulator = 0;
+
+        for (var d = lesser; d < greater; d.setDate(d.getDate() + 1)) {
+
+          // number of roommates present on this day
+          var roommate_count = Roommates.find({ arrival_date: { $lt: d }, departure_date: { $gt: d } }).count();
+          
+          // prevent division by zero
+          if (roommate_count > 0){
+            // add to accumulated total payment for user and bill
+            accumulator += ((bill.amount / roommate_count)/diffDays);
+          }
+
+        }
+
+        return accumulator;
+      }
+
+      return 0;
+    } else {
+      return 'fail';
+    }
   };
 
-  Template.grid.bills = function () {
-    return Bills.find({});
+  Template.grid.paid = function (options) {
+    payment = Payments.findOne({bill_id: options.hash.bill_id, roommate_id: options.hash.roommate_id});
+
+    if (payment){
+      return payment;
+    } else{
+      return "nada";
+    }
+    // return 'derp';
   };
 
-  Template.grid.bill_roommate = function (roommate_id, bill_id) {
-    // var roommate = Roommates.findOne(roommate_id);
-    // var bill = Bills.findOne(bill_id);
-    // return bill.name;
-    return "foo";
-  };
+  Template.payment.payer_name = function(payer_id) {
+    return Roommates.findOne(payer_id).name;
+  }
 
   Template.grid.events({
 
     'submit form.new_roommate': function(event) {
       Roommates.insert({
         name: event.target.elements.namedItem('name').value,
-        arrival_date: event.target.elements.namedItem('arrival_date').value,
-        departure_date: event.target.elements.namedItem('departure_date').value
+        arrival_date: new Date(event.target.elements.namedItem('arrival_date').value),
+        departure_date: new Date(event.target.elements.namedItem('departure_date').value),
       });
     },
 
     'submit form.new_bill': function(event) {
       Bills.insert({
         name: event.target.elements.namedItem('name').value,
-        arrival_date: event.target.elements.namedItem('arrival_date').value,
-        departure_date: event.target.elements.namedItem('departure_date').value,
+        arrival_date:new Date(event.target.elements.namedItem('arrival_date').value),
+        departure_date: new Date(event.target.elements.namedItem('departure_date').value),
+        amount: event.target.elements.namedItem('amount').value
+      });
+    },
+
+    'submit form.new_payment': function(event) {
+      console.log(event.target.elements.namedItem('payer'));
+      Payments.insert({
+        payer: event.target.elements.namedItem('payer').value,
         amount: event.target.elements.namedItem('amount').value
       });
     }
-
   });
 
   Template.roommate.events({
@@ -52,13 +105,12 @@ if (Meteor.isClient) {
     },
 
     "blur input[name='arrival_date']": function (event) {
-      Roommates.update(event.target.dataset.roommateid, {$set: {arrival_date: event.target.value}});
+      Roommates.update(event.target.dataset.roommateid, {$set: {arrival_date: new Date(event.target.value)}});
     },
     
     "blur input[name='departure_date']": function (event) {
-      Roommates.update(event.target.dataset.roommateid, {$set: {departure_date: event.target.value}});
+      Roommates.update(event.target.dataset.roommateid, {$set: {departure_date: new Date(event.target.value)}});
     }
-
   });
 
   Template.bill.events({
@@ -76,13 +128,20 @@ if (Meteor.isClient) {
     },
 
     "blur input[name='arrival_date']": function (event) {
-      Bills.update(event.target.dataset.billid, {$set: {arrival_date: event.target.value}});
+      Bills.update(event.target.dataset.billid, {$set: {arrival_date: new Date(event.target.value)}});
     },
     
     "blur input[name='departure_date']": function (event) {
-      Bills.update(event.target.dataset.billid, {$set: {departure_date: event.target.value}});
+      Bills.update(event.target.dataset.billid, {$set: {departure_date: new Date(event.target.value)}});
     }
 
+  });
+
+  Template.payment.events({
+
+    'click input.delete_payment': function (event) {
+      Payments.remove(event.target.dataset.paymentid);
+    },
   });
 
 }
